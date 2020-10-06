58d822b0053bb370d8cfc605e7855dcb12a9d249
Sebastien Deriaz <sebastien.deriaz1@gmail.com>
2020-10-06 20:55:26 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='SD_Tools',
-    version='0.2',
+    version='1.0',
     packages=['SD_Tools',],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     url='https://github.com/SebastienDeriaz/SD_Tools'
