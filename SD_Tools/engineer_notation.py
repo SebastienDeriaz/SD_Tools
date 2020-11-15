8a802a874c61cc13c57a6e3c14fa167cfe60f2cf
Sebastien Deriaz <sebastien.deriaz1@gmail.com>
2020-11-15 19:59:02 +0100

    Updated for use with hybrid text-equation style

--- SD_Tools/engineer_notation.py
+++ SD_Tools/engineer_notation.py
@@ -1,11 +1,12 @@
 from IPython.display import display, Math
 
-def to_units(prefix : str, num : float, extension : str, precision : int):
-    
+def to_units(prefix : str, num : float, extension : str, precision : int = 2):
     if(extension == "kg" or extension == "$kg$"):
         num = num * 1000
-    else:
         mag = 1
+        etension.replace("k", "")
+    else:
+        mag = 0
     
     if(num > 0):
         polarity = 0
@@ -28,14 +29,42 @@ def to_units(prefix : str, num : float, extension : str, precision : int):
     
     if(polarity):
         num = -num
-    evaluatedExpression = "%.*f %s%s" %(precision, num, indices[mag+8], extension)
-    if(prefix[0] == '$' and prefix[-1] == '$'): #latex
-        prefix = prefix.replace("$", "")
-        display(Math(r"%s\text{%s}" % (prefix, evaluatedExpression)))
-    else:
-        display(Math(r"\text{%s%s}" % (prefix, evaluatedExpression)))
+    formattedExtension = __latexDisplay(" " + indices[mag+8] + extension)
+    evaluatedExpression = "%.*f %s" %(precision, num, formattedExtension)
     
-def to_eng(prefix : str, num : float, extension : str, precision : int):
+    print("eval : ", evaluatedExpression)
+    formattedPrefix = __latexDisplay(prefix)
+    print("prefix format: ", formattedPrefix)
+    mathExpression = formattedPrefix + evaluatedExpression
+    print("final : ", mathExpression);
+    display(Math(mathExpression))
+    
+
+def __latexDisplay(strIn : str):
+    strOut = list(strIn)
+    for i in range(0, len(strIn)):
+        if(i == 0):
+            if(strIn[i] == "$"):
+                #On entre en mode équation
+                equationMode = True
+            else:
+                #On entre en mode texte
+                strOut[i] = r"\text{" + strIn[i]
+                equationMode = False
+        if(i == len(strIn)-1): #Dernier élément
+            if(not equationMode):
+                strOut[i] =  strIn[i] + "}"
+        elif(strIn[i] == "$" and i > 0):
+            if(equationMode):
+                equationMode = False
+                strOut[i] = r"\text{"
+            else:
+                equationMode = True
+                strOut[i] = "}"
+    strOut = "".join(strOut)
+    return strOut
+
+def to_eng(prefix : str, num : float, extension : str, precision : int = 2):
     mag = 0
     if(num > 0):
         polarity = 0
@@ -52,11 +81,13 @@ def to_eng(prefix : str, num : float, extension : str, precision : int):
         while(num < 1 and mag > -8):
             num*=1000
             mag-=1
+            
     if(polarity):
         num = -num
-    evaluatedExpression = r"%.*f$\cdot 10^{%d}$ %s" % (precision, num, mag*3, extension)
-    if(prefix[0] == '$' and prefix[-1] == '$'): #latex
-        prefix = prefix.replace("$", "")
-        display(Math(r"%s\text{%s}" % (prefix, evaluatedExpression)))
-    else:
-        display(Math(r"\text{%s}%s" % (prefix, evaluatedExpression)))  
+    formattedExtension = __latexDisplay(" " + extension)
+    evaluatedExpression = r"%.*f\cdot 10^{%d} %s" %(precision, num, mag*3, formattedExtension)
+    
+    formattedPrefix = __latexDisplay(prefix)
+    mathExpression = formattedPrefix + evaluatedExpression
+    print("final : ", mathExpression);
+    display(Math(mathExpression))
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='SD_Tools',
-    version='1.0',
+    version='1.1',
     packages=['SD_Tools',],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     url='https://github.com/SebastienDeriaz/SD_Tools'
